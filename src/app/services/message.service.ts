6928886b8b06c687c66595f0d029733621eb5d84
Javier Barreda <javier.barreda94@gmail.com>
2021-12-21 20:56:29 -0600

    Ultimos cambios

--- src/app/services/message.service.ts
+++ src/app/services/message.service.ts
@@ -1,7 +1,8 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; 
-
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
 
 @Injectable()
 export class MessageService {
@@ -9,8 +10,11 @@ export class MessageService {
   constructor(private _http: HttpClient) { }
 
   sendMessage(body: object) {
-    
-    return this._http.post('https://www.compus4less.com/formulario', body, {responseType: 'text'});
+     return this._http.post('https://www.compus4less.com/formulario', body, {responseType: 'text'});
   }
 
+  getBannerImages(){
+    return this._http.get('http://localhost:3005/banner');  
+  }
+   
 }
